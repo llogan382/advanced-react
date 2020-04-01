19840187d613f0dacf592dd105037a6059c449f9
Luke Logan <llogan382@gmail.com>
2020-04-01 06:10:02 -0400

    Import custom fonts

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -24,6 +24,29 @@ margin: 0 auto;
 padding: 2rem;
 `;
 
+injectGlobal`
+@font-face {
+    font-family: 'radnika_next';
+    src: url()('/static/radnikanext-medium-webfont.woff2');
+    format: ('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
+html{
+    box-sizing: border-box;
+    font-size: 10px;
+}
+*, *:before, *:after {
+    box-sizing: inherit;
+}
+body{
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+}
+`
+
 class Page extends Component {
     render() {
         return (
