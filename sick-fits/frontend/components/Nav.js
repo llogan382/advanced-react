3e162ad130d5a58d9569fb52b9ef2f6cb3150420
Luke <llogan382@gmail.com>
2020-06-01 06:38:26 -0400

    Update Nav menu

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,14 +1,26 @@
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 const Nav = () => (
-    <div>
-        <Link href="/sell">
-            <a>Sell!</a>
+    <NavStyles>
+        <Link href="items">
+            <a>items</a>
         </Link>
-        <Link href="/">
-            <a>Home!</a>
+        <Link href="sell">
+            <a>sell</a>
         </Link>
-    </div>
-)
+        <Link href="signup">
+            <a>signup</a>
+        </Link>
+        <Link href="orders">
+            <a>orders</a>
+        </Link>
+        <Link href="me">
+            <a>me</a>
+        </Link>
+    </NavStyles>
+);
+
+
 
 export default Nav;
