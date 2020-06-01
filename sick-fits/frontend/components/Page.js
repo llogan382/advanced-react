f220527dca615fef039b2e2fffa5f1de7c28ae59
Luke <llogan382@gmail.com>
2020-06-01 06:52:44 -0400

    Update style layout

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header';
 import Meta from './Meta';
-import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
-// Use a theme to create global styles
 const theme = {
     red: '#FF0000',
     black: '#393939',
@@ -15,32 +14,55 @@ const theme = {
 };
 
 const StyledPage = styled.div`
- background: white;
- color: ${props => props.theme.black};
+  background: white;
+  color: ${props => props.theme.black};
 `;
 
 const Inner = styled.div`
-max-width: ${props => props.theme.maxWidth};
-margin: 0 auto;
-padding: 2rem;
-background: ${props => props.theme.red};
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
 `;
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+
 class Page extends Component {
     render() {
         return (
             <ThemeProvider theme={theme}>
-
                 <StyledPage>
                     <Meta />
                     <Header />
-                    <Inner>
-                        {this.props.children}
-                    </Inner>
+                    <Inner>{this.props.children}</Inner>
                 </StyledPage>
             </ThemeProvider>
         );
     }
 }
 
-// Use this in the APP component
 export default Page;
