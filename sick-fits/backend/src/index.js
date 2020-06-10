e59b29153de5a7430ed5abc6dc5c97073f0a3c59
Luke <llogan382@gmail.com>
2020-06-10 05:56:32 -0400

    Pull in all data. Entrypoint for our backend

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,25 @@
 // let's go!
+// This will start our node server
+// This is entry point for the application
+require('dotenv').config({ path: 'variables.env' });
+
+const createServer = require('./createServer');
+
+// Import DB we creates
+const db = require('./db');
+
+const server = createServer();
+
+// TODO Use express middleware to populate cookies (JWT)
+// TODO Use express middleware to populate current user
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    },
+},
+    deets => {
+        console.log(`Server is now running on port http://localhost:${deets.port}`);
+    }
+);
