6c2117c87ab93265b61721f69e802601ed9e5a70
Luke <llogan382@gmail.com>
2020-06-19 06:12:25 -0400

    Uploading images to cloudinary was enabled

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -26,6 +26,7 @@ createItem(
     }
 `;
 
+
 class CreateItem extends Component {
 
     state = {
@@ -41,6 +42,26 @@ class CreateItem extends Component {
         const val = type === 'number' ? parseFloat(value) : value;
         this.setState({ [name]: val });
     }
+
+
+    uploadFile = async e => {
+        console.log('uploading file');
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'sickfits');
+
+        const res = await fetch('https://api.cloudinary.com/v1_1/lwd-loganwebdev/image/upload', {
+            method: 'POST',
+            body: data
+        });
+        const file = await res.json();
+        console.log(file);
+        this.setState({
+            image: file.secure_url,
+            largeImage: file.eager[0].secure_url
+        })
+    };
 
     render() {
         return (
@@ -60,6 +81,18 @@ class CreateItem extends Component {
                         }}>
                         <Error error={error} />
                         <fieldset disabled={loading} aria-busy={loading}>
+
+                            <label htmlFor="file">
+                                Image
+                        <input type="file" id="file" name="file" placeholder="Upload an image"
+                                    // required
+                                    onChange={this.uploadFile}
+                                />
+                                {
+                                    this.state.image && <img src={this.state.image} alt="Upload Preview" />
+                                }
+                            </label>
+
                             <label htmlFor="title">
                                 Title
                         <input type="text" id="title" name="title" placeholder="Title" required value={this.state.title}
