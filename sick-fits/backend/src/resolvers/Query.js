8598dfd6417d3486b7dbe899e48322eb74f707de
Luke <llogan382@gmail.com>
2020-06-22 07:16:42 -0400

    Update graphql query

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -3,7 +3,9 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
 
     // If you just want to query the DB without auth or anything, just get all info from DB:
-    items: forwardTo('db')
+    items: forwardTo('db'),
+    item: forwardTo('db'),
+
 
     // async items(parent, args, ctx, info) {
     //     const items = await ctx.db.query.items();
