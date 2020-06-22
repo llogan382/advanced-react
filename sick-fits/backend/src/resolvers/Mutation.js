401c8a7228db606fe2741cfe9066cc53fb690ac7
Luke <llogan382@gmail.com>
2020-06-22 07:16:54 -0400

    Add a mutation function

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -13,7 +13,24 @@ const mutations = {
         }, info);
 
         return item;
-    }
+    },
+    updateItem(parent, args, ctx, info) {
+        // take a copy of the updates
+        const updates = { ...args };
+
+        // remove ID from the updates
+        delete updates.id;
+
+        // run the update method
+        return ctx.db.mutation.updateItem({
+            data: updates,
+            where: {
+                id: args.id
+            },
+        },
+            info
+        );
+    },
 };
 
 module.exports = mutations;
