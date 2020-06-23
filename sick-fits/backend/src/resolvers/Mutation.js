3936bd834c91a8acf608a1d78e15485af178b429
Luke <llogan382@gmail.com>
2020-06-23 06:09:49 -0400

    Add a function to await db mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -31,6 +31,22 @@ const mutations = {
             info
         );
     },
+    async deleteItem(parent, args, ctx, info) {
+        const where = { id: args.id };
+
+        // 1. Find the item
+        const item = await ctx.db.query.item({ where }, `{ id title}`);
+
+        // 2. Check if they own the item (permissions)
+        // TODO
+
+        // 3. Delete it.
+        return ctx.db.mutation.deleteItem({ where }, info);
+
+    }
+
+
+
 };
 
 module.exports = mutations;
