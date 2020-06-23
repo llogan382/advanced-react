6bba1121a22a7ff4aa0a4ce9e2d8540d143e7abc
Luke <llogan382@gmail.com>
2020-06-23 06:08:59 -0400

    Import the component that was created

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -5,6 +5,8 @@ import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import Link from 'next/link';
 import formatMoney from '../lib/formatMoney';
+import DeleteItem from './DeleteItem';
+
 export default class Item extends Component {
     static propTypes = {
         prop: PropTypes.shape({
@@ -43,7 +45,7 @@ export default class Item extends Component {
                         <a>Edit </a>
                     </Link>
                     <button>Add to Cart</button>
-                    <button>Delete</button>
+                    <DeleteItem id={item.id}>Delete This Item</DeleteItem>
                 </div>
             </ItemStyles>
         )
