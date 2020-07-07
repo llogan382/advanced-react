40d00486ac970ba5233deb9122782701698e4820
Luke <llogan382@gmail.com>
2020-07-07 06:23:25 -0400

    Import query component. No restart. Works.

--- sick-fits/frontend/components/Pagination.js
+++ sick-fits/frontend/components/Pagination.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import PaginationStyles from './styles/PaginationStyles';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+
+const PAGINATION_QUERY = gql`
+    query PAGINATION_QUERY {
+        itemsConnection {
+            aggregate {
+                count
+            }
+        }
+    }
+`;
 
 const Pagination = props => (
     <PaginationStyles>
