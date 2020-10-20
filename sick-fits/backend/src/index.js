9a57f0e84990b7bec107bf52a1bfc8a642ab075a
Luke <llogan382@gmail.com>
2020-10-20 06:08:48 -0400

    re-do getting yoga server setup

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,22 +1,23 @@
 // let's go!
 // This will startup our node server
 // Make sure .env files are available
-
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
+
 const server = createServer();
 
-// todo: use express middleware to handle coookies
-// todo: use express middleware to populate current user
+// TODO Use express middlware to handle cookies (JWT)
+// TODO Use express middlware to populate current user
 
-
-server.start({
-
-    cors: {
-        credentials: true,
-        origin: process.env.FRONTEND_URL,
+server.start(
+    {
+        cors: {
+            credentials: true,
+            origin: process.env.FRONTEND_URL,
+        },
     },
-}, deets => {
-    console.log(`server is now running on port http://localhost:${deets.port}`);
-});
+    deets => {
+        console.log(`Server is now running on port http:/localhost:${deets.port}`);
+    }
+);
