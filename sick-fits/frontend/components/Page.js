acc224e58bb872ec45ddaed9b5e916158e22d4d3
Luke <llogan382@gmail.com>
2021-04-30 11:04:43 -0400

    finishes global styles; fixes server caching bug

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -51,7 +51,7 @@ const InnerStyles = styled.div`
   padding: 2rem;
 `;
 
-export default function Page({ children, cool }) {
+export default function Page({ children }) {
   return (
     <div>
       <GlobalStyles />
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,12 @@
+import Nprogress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+import '../components/styles/nprogress.css';
+
+Router.events.on('routerChangeStart', () => Nprogress.start());
+Router.events.on('routerChangeComplete', () => Nprogress.done());
+Router.events.on('routerChangeComplete', () => Nprogress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
