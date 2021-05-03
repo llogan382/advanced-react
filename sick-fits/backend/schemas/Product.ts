17c0823c6a18a8687791e7c72f3f8a91c267aa90
Luke <llogan382@gmail.com>
2021-05-03 11:10:47 -0400

    Adds product field and a few options

--- sick-fits/backend/schemas/Product.ts
+++ sick-fits/backend/schemas/Product.ts
@@ -1,4 +1,4 @@
-import { text } from '@keystone-next/fields';
+import { integer, select, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 
 export const Product = list({
@@ -11,5 +11,19 @@ export const Product = list({
         displayMode: 'textarea',
       },
     }),
+    status: select({
+      options: [
+        { label: 'Draft', value: 'DRAFT' },
+        { label: 'Avalable', value: 'AVALABLE' },
+        { label: 'Unavailable', value: 'UNAVAILABLE' },
+      ],
+      defaultValue: 'DRAFT',
+      ui: {
+        displayMode: 'segmented-control',
+        createView: { fieldMode: 'hidden' },
+      },
+    }),
+    price: integer(),
+    // TODO: photo
   },
 });
