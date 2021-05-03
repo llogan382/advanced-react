2e795013487b81a1c997b675b56f8be54b526da0
Luke <llogan382@gmail.com>
2021-05-03 10:48:32 -0400

    Updates IMPORT and adds AUTH

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,8 +1,14 @@
-import { User } from './schemas/User';
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
 import 'dotenv/config';
-import { config, createSchema } from '@keystone-next/keystone/schema'; // import items from keystone
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
 
 // define db
+
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -12,25 +18,44 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'], // when setting up the first user, what roles will they have?
+    // TODO: Add in initial roles.
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    // TODO: Addd data seeding here
-  },
-  lists: createSchema({
-    // schema items fo in here
-    User,
-  }),
-  ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
-  },
-  // TODO: Add session value here.
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: Addd data seeding here
+    },
+    lists: createSchema({
+      // schema items fo in here
+      User,
+    }),
+    ui: {
+      // TODO: change this for roles
+      // Show UI only for ppl logged in:
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id name email',
+    }),
+  })
+);
