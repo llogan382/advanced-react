046be0654cf956210156f1b3e6853f2c882ebf02
Luke <llogan382@gmail.com>
2021-05-10 06:07:30 -0400

    two way data binding w images

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -7,6 +7,7 @@ import {
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
 
 // define db
 
@@ -46,6 +47,7 @@ export default withAuth(
       // schema items fo in here
       User,
       Product,
+      ProductImage,
     }),
     ui: {
       // TODO: change this for roles
