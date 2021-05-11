4a2319dac8ad48f49fc068dc97d5213169147ff8
Luke <llogan382@gmail.com>
2021-05-11 05:48:19 -0400

    sets up apollo client on frontend

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -5,8 +5,10 @@ import { createUploadLink } from 'apollo-upload-client';
 import withApollo from 'next-with-apollo';
 import { endpoint, prodEndpoint } from '../config';
 
+// create the apollo client.
 function createClient({ headers, initialState }) {
   return new ApolloClient({
+    // inject the first link: for errors.
     link: ApolloLink.from([
       onError(({ graphQLErrors, networkError }) => {
         if (graphQLErrors)
@@ -15,14 +17,18 @@ function createClient({ headers, initialState }) {
               `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
             )
           );
+        // Insert link if there are network errors.
         if (networkError)
           console.log(
             `[Network error]: ${networkError}. Backend is unreachable. Is it running?`
           );
       }),
       // this uses apollo-link-http under the hood, so all the options here come from that package
+      // fetch, update, create new items.
+      // this is from Apollo-link-http, a separate package.
       createUploadLink({
         uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
+        // do we want to include cookies on http requests?
         fetchOptions: {
           credentials: 'include',
         },
@@ -30,6 +36,7 @@ function createClient({ headers, initialState }) {
         headers,
       }),
     ]),
+    // To help users load pages quickly
     cache: new InMemoryCache({
       typePolicies: {
         Query: {
@@ -39,8 +46,10 @@ function createClient({ headers, initialState }) {
           },
         },
       },
+      // If there are changes
     }).restore(initialState || {}),
   });
 }
 
+// Get data from tree: Crawl components, and look for changes, and wait for data to be fetched before serving the HTML.
 export default withApollo(createClient, { getDataFromTree });
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,17 +1,34 @@
 import Nprogress from 'nprogress';
 import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
-
 import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
 
 Router.events.on('routerChangeStart', () => Nprogress.start());
 Router.events.on('routerChangeComplete', () => Nprogress.done());
 Router.events.on('routerChangeComplete', () => Nprogress.done());
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
+  console.log(apollo);
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+// If any other pages have get initialProps, we will wait and go and fetch it.
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+// Give me the app, and include the data inside of it
+export default withData(MyApp);
